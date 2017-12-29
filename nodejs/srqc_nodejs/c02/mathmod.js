8122fee07a480446a7de0dc0166533d3671c5eba
cypeng <286816680@qq.com>
2017-12-29 11:21:40 +0800

    mathmod.js export sub function

--- nodejs/srqc_nodejs/c02/mathmod.js
+++ nodejs/srqc_nodejs/c02/mathmod.js
@@ -2,3 +2,8 @@ exports.add = function (n1, n2) {
 	var ret = n1 + n2;
 	return ret;
 };
+
+exports.sub = function (n1, n2) {
+	var ret = n1 - n2;
+	return ret;
+};
--- nodejs/srqc_nodejs/c02/mathtest.js
+++ nodejs/srqc_nodejs/c02/mathtest.js
@@ -1,6 +1,9 @@
 var mathmod = require('./mathmod');
 var n1 = 5;
 var n2 = 10;
-var n = mathmod.add(n1, n2);
 
-console.log("n", n);
+var r1 = mathmod.add(n1, n2);
+console.log("r1", r1);
+
+var r2 = mathmod.sub(n1, n2);
+console.log("r2", r2);
