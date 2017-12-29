68b64176c026905abb259db99dfa5ad827780461
cypeng <286816680@qq.com>
2017-12-29 11:16:30 +0800

    c02 first commit

---
+++ nodejs/srqc_nodejs/c02/mathmod.js
@@ -0,0 +1,4 @@
+exports.add = function (n1, n2) {
+	var ret = n1 + n2;
+	return ret;
+};
---
+++ nodejs/srqc_nodejs/c02/mathtest.js
@@ -0,0 +1,6 @@
+var mathmod = require('./mathmod');
+var n1 = 5;
+var n2 = 10;
+var n = mathmod.add(n1, n2);
+
+console.log("n", n);
