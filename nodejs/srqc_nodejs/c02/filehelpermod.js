e2161e3cb508685497891af54035a288229173cd
cypeng <286816680@qq.com>
2017-12-29 16:15:59 +0800

    filehelpermod add load func

--- nodejs/srqc_nodejs/c02/filehelpermod.js
+++ nodejs/srqc_nodejs/c02/filehelpermod.js
@@ -1,18 +1,16 @@
 //FileHelper
+var fs = require('fs');
+
 function FileHelper(path) {
 	this.path =  path;
 };
 
-FileHelper.prototype.getPath = function () {
-	return this.path;
-};
-
-FileHelper.prototype.setPath = function (path) {
-	this.path = path;
-};
-
 FileHelper.prototype.load = function (cb) {
 	console.log('FileHelper.prototype.load', this.path);
+
+	fs.readFile(this.path, function (err, data) {
+		cb(err, data);
+	});
 };
 
 exports.createFileHelper = function (path) {
--- nodejs/srqc_nodejs/c02/filehelpertest.js
+++ nodejs/srqc_nodejs/c02/filehelpertest.js
@@ -1,7 +1,7 @@
 var filehelpermod = require('./filehelpermod');
 
 function fh1_load_cb(err, data) {
-	console.log("fh1_load_cb", err, data);
+	console.log("fh1_load_cb", err, data, data.toString());
 };
 var fh1 = filehelpermod.createFileHelper('./fh1.txt');
 fh1.load(fh1_load_cb);
