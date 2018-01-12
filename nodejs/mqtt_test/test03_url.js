8a4ac6d99448e2ae721d92ac14f5ae4f89a0e5d0
cypeng001 <286816680@qq.com>
2018-01-12 15:48:33 +0800

    test for mqtt

---
+++ nodejs/mqtt_test/test03_url.js
@@ -0,0 +1,8 @@
+var mqtt = require('mqtt')
+  , client = mqtt.connect('mqtt://user:pass@localhost?clientId=123abc');
+
+client.subscribe('messages');
+client.publish('messages', 'hello me!');
+client.on('message', function(topic, message) {
+  console.log(message);
+});
